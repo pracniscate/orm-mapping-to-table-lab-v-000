5b8d1cba72c824b30ff711f53e8e6b1a6c7e9c69
Lorena <lorena.giddens@outlook.com>
2018-07-18 13:38:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id = nil)
-    @name = name 
-    @grade = grade 
+    @name = name
+    @grade = grade
     @id = id
   end
-  
+
 end
